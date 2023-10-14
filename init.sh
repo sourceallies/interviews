89c0a49b372750c1a81949d431c6913ccf0e0d7c
Brennan Seymour <brenno263@gmail.com>
2023-10-14 15:55:49 +0000

    replace cat with head

--- init.sh
+++ init.sh
@@ -5,7 +5,8 @@ pip install inquirer
 # Since we need to present a user interface through stdout, we write
 # output to fd 3, redirect it to a file, and cat that out to a variable.
 python ./languageSelect.py 3> /tmp/language_select_out
-SELECTION=$(cat /tmp/language_select_out)
+# We use head to ensure that we never read more than one line.
+SELECTION=$(head -1 /tmp/language_select_out)
 
 cd $SELECTION
 make init
