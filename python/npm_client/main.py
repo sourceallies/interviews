ad76ddd08b684812c787d94f3d88e0d0fee95569
JP Shilton <jp.shilton@openphone.co>
2024-06-14 16:38:21 -0400

    display raw errors

--- python/npm_client/main.py
+++ python/npm_client/main.py
@@ -8,16 +8,11 @@ def run():
     if len(sys.argv) < 2:
         print("Usage: python npm_client.py <package-name>")
     package_name = sys.argv[1]
-    try:
-        dependencies = asyncio.run(list_transitive_dependencies(package_name))
-        print(f"Transitive dependencies of: {package_name}:")
-        for dep in dependencies:
-            print(f"  - {dep}")
-    except Exception as e:
-        print(e)
+
+    dependencies = asyncio.run(list_transitive_dependencies(package_name))
+    print(f"Transitive dependencies of: {package_name}:")
+    for dep in dependencies:
+        print(f"  - {dep}")
 
 if __name__ == "__main__":
-    try:
-        run()
-    except Exception as error:
-        print(error)
+    run()
