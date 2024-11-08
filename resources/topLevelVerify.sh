b4b1a0fcd35087455edfe36b510cffb6dab3c166
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2024-11-07 21:47:02 -0600

    Fail build if verify script fails

--- resources/topLevelVerify.sh
+++ resources/topLevelVerify.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+set -e
+
 for dir in */
 do    
-    make init -C $dir
-    make verify -C $dir
+    if [ "$dir" == "resources/" ]; then
+        continue
+    fi
+
+    make init -C "$dir"  
+    make verify -C "$dir"
 done
