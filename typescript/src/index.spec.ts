73d80e0af858cbc642f0d444e57f4693fd7eb640
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-03-05 20:00:10 +0000

    ses

--- typescript/src/index.spec.ts
+++ typescript/src/index.spec.ts
@@ -1,7 +1,143 @@
-import { greet } from "./index";
+import { calculateFrameScore, calculateRunningScore, getNextRollScore } from "./index";
 
-describe("greet", () => {
-  it("should greet by name", () => {
-    expect(greet("World")).toBe("Hello, World!");
+describe("Bowling Game", () => {
+  describe("Calculate Score By Frame", () => {
+    it("Get frame score for no pins hit", () => {
+      expect(calculateFrameScore({ pinsHit: [0, 0] })).toBe(0);
+    });
+    it("Get frame score for 9 pins hit", () => {
+      expect(calculateFrameScore({ pinsHit: [9, 0] })).toBe(9);
+    });
+    it("Get frame score for 10 pins hit on both rolls", () => {
+      expect(calculateFrameScore({ pinsHit: [1, 9] })).toBe('/');
+    });
+    it("Get frame score for 10 pins hit on first roll", () => {
+      expect(calculateFrameScore({ pinsHit: [10, 0] })).toBe('X');
+    });
+    it("Get frame score for final frame", () => {
+      expect(calculateFrameScore({ pinsHit: [5, 5, 9] })).toBe(19);
+    });
+    it("Get frame score for final frame - variant", () => {
+      expect(calculateFrameScore({ pinsHit: [10, 5, 3] })).toBe(18);
+    });
+  });
+});
+describe("Calculate Score By Multi-Frames", () => {
+  it("Get running score for 2 frames thrown", () => {
+    const input = [
+      { pinsHit: [0, 0] },
+      { pinsHit: [0, 0] }
+    ];
+
+    expect(calculateRunningScore(input)).toBe(0);
+  });
+  it("Get running score for 5 frames thrown - no strikes or spares", () => {
+    const input = [
+      { pinsHit: [5, 0] },
+      { pinsHit: [0, 5] },
+      { pinsHit: [2, 2] },
+      { pinsHit: [1, 2] },
+      { pinsHit: [2, 0] }
+    ];
+
+    expect(calculateRunningScore(input)).toBe(19);
+  });
+  it("Get running score for 5 frames thrown - one spare", () => {
+    const input = [
+      { pinsHit: [5, 0] },
+      { pinsHit: [0, 5] },
+      { pinsHit: [8, 2] },
+      { pinsHit: [1, 2] },
+      { pinsHit: [2, 0] }
+    ];
+
+    expect(calculateRunningScore(input)).toBe(26);
+  });
+  it("Get running score for 5 frames thrown - one strike", () => {
+    const input = [
+      { pinsHit: [5, 0] },
+      { pinsHit: [0, 5] },
+      { pinsHit: [10] },
+      { pinsHit: [1, 2] },
+      { pinsHit: [2, 0] }
+    ];
+
+    expect(calculateRunningScore(input)).toBe(28);
+  });
+  it("Get running score for 6 frames thrown - multiple strike", () => {
+    const input = [
+      { pinsHit: [10] }, // X + X (2) + X (3) => 30
+      { pinsHit: [10] }, // X + X (3) + X (4) => 30
+      { pinsHit: [10] }, // X + X (4) + X (5)  => 30
+      { pinsHit: [10] }, // X + X (5) + 5  => 25
+      { pinsHit: [10] }, // X + 5 + 4
+      { pinsHit: [5, 4] }, // 9
+    ];
+
+    expect(calculateRunningScore(input)).toBe(143);
+  });
+  it("Get running score for 10 frames thrown - standard end frame ", () => {
+    const input = [
+      { pinsHit: [10] }, // X + X (2) + X (3) => 30
+      { pinsHit: [10] }, // X + X (3) + X (4) => 30
+      { pinsHit: [10] }, // X + X (4) + X (5)  => 30
+      { pinsHit: [10] }, // X + X (5) + 5  => 25
+      { pinsHit: [10] }, // X + 5 + 4
+      { pinsHit: [5, 4] }, // 9
+      { pinsHit: [5, 4] }, // 9
+      { pinsHit: [5, 4] }, // 9
+      { pinsHit: [5, 4] }, // 9
+      { pinsHit: [5, 4] }, // 9
+    ];
+
+    expect(calculateRunningScore(input)).toBe(179);
+  });
+  it("Get running score for 10 frames thrown - spare end frame ", () => {
+    const input = [
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 5, 9] },
+    ];
+
+    expect(calculateRunningScore(input)).toBe(189);
+  });
+  it("Get running score for 10 frames thrown - strike end frame ", () => {
+    const input = [
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [5, 4] },
+      { pinsHit: [10, 5, 3] },
+    ];
+
+    expect(calculateRunningScore(input)).toBe(188);
+  });
+  it("Get running score for perfect game", () => {
+    const input = [
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10] },
+      { pinsHit: [10, 10, 10] },
+    ];
+
+    expect(calculateRunningScore(input)).toBe(300);
   });
 });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,94 @@
-export const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+
+/*
+  X X X X
+   X X X
+    X X
+     X
+
+  10 pins total
+  assume input is valid
+
+  Scoring
+The score for each frame is based on the number of pins you knock down.
+
+A simple roll is when you use both balls in a frame and do not knock down all 10 pins. Your score for that frame is the total number of pins knocked down.
+A Strike (X) is when you knock down all 10 pins with your first ball. You will not roll a second ball for this frame. Your score for this strike frame is 10 plus the number of pins knocked down on your next two rolls.
+A Spare (/) is when you knock down all 10 pins using both of your rolls in a frame. Your score for this spare frame is 10 plus the number of pins knocked down on your next roll.
+
+A Strike (X) in frame 10 with the first ball. If you get a strike with the first ball in frame 10 you will roll 2 more balls. This rule is only for frame 10!
+A Spare (/) in frame 10 If you get a spare in frame 10 with your first 2 balls you will roll one more ball. This rule is only for frame 10!
+The maximum score for a perfect game is 300.
+*/
+
+interface FrameInput {
+  pinsHit: number[],
+}
+
+
+
+export function calculateFrameScore(input: FrameInput): number | 'X' | '/' {
+  let frameScore: number = 0;
+  const isLastFrame = input.pinsHit.length === 3;
+  let response: number | 'X' | '/';
+
+  for (let i = 0; i < input.pinsHit.length; i++) {
+    const roll = input.pinsHit[i];
+    if (i === 0 && roll === 10 && !isLastFrame) {
+      return 'X';
+    } else {
+      frameScore += roll;
+      response = frameScore;
+    }
+    if (frameScore === 10 && !isLastFrame) {
+      return '/'
+    }
+  }
+
+  return response;
+}
+
+export function calculateRunningScore(input: FrameInput[]): number {
+  let totalScore = 0;
+  let isLastFrame = false;
+  input.forEach((i: FrameInput, index: number) => {
+    if (index === 9) isLastFrame = true;
+    const currentFrameScore = calculateFrameScore(i);
+    const nextRoll = input[index + 1];
+    if (currentFrameScore === '/') {
+      if (isLastFrame) {
+        totalScore += Number(currentFrameScore);
+      } else {
+        totalScore += 10 + nextRoll.pinsHit[0];
+      }
+    } else if (currentFrameScore === 'X') {
+      if (isLastFrame) {
+        totalScore += Number(currentFrameScore);
+      } else {
+        totalScore += 10 + nextRoll.pinsHit[0];
+        if (nextRoll.pinsHit[1]) {
+          totalScore += nextRoll.pinsHit[1]
+        } else {
+          const nextNextRoll = input[index + 2];
+          totalScore += nextNextRoll.pinsHit[0];
+        }
+      }
+    } else {
+      totalScore += Number(currentFrameScore);
+    }
+  });
+  return totalScore;
+}
+
+export function getNextRollScore(input: FrameInput[], currentFrameIndex: number, currentRoll: 'X' | '/'): number {
+  let result = 0;
+  const nextScore = calculateFrameScore(input[currentFrameIndex + 1]);
+  console.log(nextScore);
+  if (nextScore === 'X') {
+    const val = getNextRollScore(input, currentFrameIndex + 1, 'X');
+    console.log(val)
+    result += val;
+  } else {
+    result = (10 + input[currentFrameIndex + 1].pinsHit[0] + (currentRoll === 'X' ? input[currentFrameIndex + 1].pinsHit[1] : 0));
+  }
+  return result;
+}
