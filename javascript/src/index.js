7eb4a1e7f893b5e278cc24dd9ed069134fa90025
jdoubleyou <jwilliamson@sourceallies.com>
2026-03-06 22:47:38 +0000

    sentencer code

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -2,6 +2,25 @@ function greet(name) {
     return `Hello, ${name}!`;
 }
 
+
+//Requirements : Take two numberics with a operator as a word between them and calculate the expression
+// eg 5 plus 5 outputs 10
+
+function sentencer(expression){
+    const operatorMap = {
+        "sum" : (x,y) => Number(x) + Number(y),
+        "plus" : (x,y) => Number(x) + Number(y),
+        "minus" : (x,y) => Number(x) - Number(y),
+        "subtract" : (x,y) => Number(x) - Number(y),
+        "divide" : (x,y) => Number(x) / Number(y)
+    }
+    const sanitized = expression.replace(/by/i,'');
+    const parts = sanitized.split(/\s+/);
+
+    return operatorMap[parts[1]]?.(parts[0], parts[2]) ?? 'Do it again dummy';
+}
+
 module.exports = {
-    greet
+    greet,
+    sentencer
 };
--- javascript/src/index.spec.js
+++ javascript/src/index.spec.js
@@ -1,8 +1,32 @@
 const { greet } = require('./index');
+const { sentencer } = require('./index');
 
 describe('greet', () => {
   it('should greet by name', () => {
     const greeting = greet('World');
     expect(greeting).toBe('Hello, World!');
   });
+});
+
+describe('sentencer', () => {
+  it.each([['5 plus 5', 10], ['6 plus 7', 13], ['6 sum 8', 14], ['6  plus     8', 14]])('Testing sums of different inputs %s = %s', (x, y) =>{
+    const sum = sentencer(x);
+    expect(sum).toBe(y);
+  });
+
+  it.each([['5 minus 5', 0], ['16 subtract 8', 8], ['6 subtract 8', -2], ['6  minus     10', -4]])('Testing subtractions of different inputs %s = %s', (x, y) =>{
+    const sum = sentencer(x);
+    expect(sum).toBe(y);
+  });
+
+  it.each([['5 divide by 5', 1], ['16 divide by 8', 2], ['6 divide by 8', 0.75], ['6  divide by     10', 0.60]])('Testing subtractions of different inputs %s = %s', (x, y) =>{
+    const sum = sentencer(x);
+    expect(sum).toBe(y);
+  });
+
+  it('should tell you do it again dummy, when the expression could not be parsed', () => {
+    const sum = sentencer('5 5 plus');
+    expect(sum).toBe('Do it again dummy');
+  });
+
 });
